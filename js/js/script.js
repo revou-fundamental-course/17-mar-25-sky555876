666c8695d04c986fc18cb337ce5b2e68adc642ff
sky555876 <tamapolo91@gmail.com>
2025-03-24 23:17:07 +0700

    menambahkan imput namber

---
+++ js/js/script.js
@@ -0,0 +1,8 @@
+function perhitunganPerkalian() {
+    let bilanganPertama = document.getElementById("").Value;
+    let bilanganKedua = document.getElementById("").Value;
+
+        let hasilPerkalian = parseInt(bilanganPertama) * parseInt(bilanganKedua);
+    
+    console.log(hasil)
+}
